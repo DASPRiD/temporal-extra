40e2055560d02f4a2fbe11e577a105ed9776cf77
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2025-11-13 16:30:21 +0100

    feat: add NOON and MIDNIGHT constants

--- src/consts.ts
+++ src/consts.ts
@@ -5,3 +5,6 @@ export const THURSDAY = 4;
 export const FRIDAY = 5;
 export const SATURDAY = 6;
 export const SUNDAY = 7;
+
+export const NOON = Temporal.PlainTime.from({ hour: 12 });
+export const MIDNIGHT = Temporal.PlainTime.from({ hour: 0 });
